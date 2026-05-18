247c069ca5de7d8c9778179c05ef1a124c80199a
ethannCB <ethann.campionbaux@gmail.com>
2026-05-18 11:35:14 +0200

    Automatic update with temperature+conditions+city

---
+++ Controle/meteo.sql
@@ -0,0 +1,75 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : lun. 18 mai 2026 à 11:33
+-- Version du serveur : 10.4.32-MariaDB
+-- Version de PHP : 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `meteos`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `meteo`
+--
+
+CREATE TABLE `meteo` (
+  `id` int(32) NOT NULL,
+  `ville` varchar(100) NOT NULL,
+  `temperature` float NOT NULL,
+  `conditions` varchar(255) NOT NULL,
+  `date` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `meteo`
+--
+
+INSERT INTO `meteo` (`id`, `ville`, `temperature`, `conditions`, `date`) VALUES
+(1, 'Paris', 14.5, 'Pluvieux', '2026-05-18 08:00:00'),
+(2, 'Lyon', 19, 'Ensoleillé', '2026-05-18 09:00:00'),
+(3, 'Perpignan', 35, 'Nuageux', '2026-05-18 08:00:00'),
+(4, 'Dijon', 12, 'neige', '2026-05-18 09:00:00'),
+(5, 'Marseille', 14.5, 'Pluvieux', '2026-05-18 08:00:00'),
+(6, 'Nice', 30, 'Ensoleillé', '2026-05-18 09:00:00'),
+(7, 'Monaco', 25, 'Pluvieux', '2026-05-18 08:00:00'),
+(8, 'Toulouse', 19, 'Ensoleillé', '2026-05-18 09:00:00');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `meteo`
+--
+ALTER TABLE `meteo`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `meteo`
+--
+ALTER TABLE `meteo`
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
